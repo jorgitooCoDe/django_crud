1cb4b8ceb6ba31e531598f6dd81ad9f5c2fb266a
DevJorgito <jorgito.soh@gmail.com>
2024-09-30 12:05:00 -0500

    testing build 2.0

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-set -e
+set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
 python manage.py migrate
--- drfcrud/settings.py
+++ drfcrud/settings.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
