31442287b7d8232c2b95c454ec66525470dccca2
DevJorgito <jorgito.soh@gmail.com>
2024-09-30 12:15:16 -0500

    change STATIC_ROOT

--- drfcrud/settings.py
+++ drfcrud/settings.py
@@ -127,9 +127,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
